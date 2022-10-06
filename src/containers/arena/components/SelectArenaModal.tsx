577ef0399cb665d0845fda3dc7e9bfea3c072891
Matthieu <matthieu@klub.ki>
2022-10-06 11:28:23 +0200

    fix : handle subscribe to league with an other name than training

--- src/containers/arena/components/SelectArenaModal.tsx
+++ src/containers/arena/components/SelectArenaModal.tsx
@@ -137,7 +137,9 @@ const ArenaContainer: React.FC<{
                 'Coming soon'
               )}
             </p>
-            <p className="mt-[16px] text-[20px] font-semibold text-[#9FA4DD]">Championship starts in</p>
+            <p className="mt-[16px] text-[20px] font-semibold text-[#9FA4DD]">
+              Championship starts in
+            </p>
           </div>
         </>
       ) : null}
