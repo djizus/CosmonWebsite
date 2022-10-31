2d8687a6f3e79b6071c4113a5fc352683f8f8652
Matthieu <matthieu@klub.ki>
2022-10-31 15:32:38 +0100

    feat : add weekly raffle

--- src/containers/arena/data.ts
+++ src/containers/arena/data.ts
@@ -3,8 +3,12 @@ import { MEAType } from 'types/MEA'
 const MEA_BASE_PATH = '/mea/'
 
 export const getMEAs = (): MEAType[] => [
+  // {
+  //   imgSrc: MEA_BASE_PATH + 'mea-whats-next.png',
+  //   link: 'https://medium.com/ki-foundation/cosmon-whats-next-2154af7de5be',
+  // },
   {
-    imgSrc: MEA_BASE_PATH + 'mea-whats-next.png',
-    link: 'https://medium.com/ki-foundation/cosmon-whats-next-2154af7de5be',
+    imgSrc: '/raffle/raffle-uncommon.png',
+    link: 'https://www.stargaze.zone/launchpad/stars1e42datgky9emwfryqp2n82lsglvqa2tpsx8m7ljfgqhsd8epxvzsaxzrtn',
   },
 ]
