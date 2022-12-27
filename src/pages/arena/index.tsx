3f9926ef521e82c293ce347a98b0aad582947cbc
podotki <arnaud.polette@gmail.com>
2022-12-27 09:38:18 +0100

    feat: add email modal on arena page

--- src/pages/arena/index.tsx
+++ src/pages/arena/index.tsx
@@ -1,13 +1,31 @@
 import Layout from '@components/Layout/Layout'
 import Arena from '@containers/arena'
+import { useLocalStorage } from '@hooks/useLocalStorage'
 import { useRouter } from 'next/router'
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
 import { useMount } from 'react-use'
+import {
+  EMAIL_COLLECTED_LOCAL_STORAGE_KEY,
+  EMAIL_LATER_COUNT_LOCAL_STORAGE_KEY,
+} from '@utils/constants'
+import EmailModal from '@components/Modal/EmailModal/EmailModal'
 
 const ARENA_IS_ACTIVE = Boolean(process.env.NEXT_PUBLIC_ARENA_IS_ACTIVE)
 
 export default function Page() {
   const router = useRouter()
+  const { getItem } = useLocalStorage()
+
+  const [displayEmailModal, setDisplayEmailModal] = useState(false)
+
+  useEffect(() => {
+    if (
+      !getItem(EMAIL_COLLECTED_LOCAL_STORAGE_KEY) &&
+      parseInt(getItem(EMAIL_LATER_COUNT_LOCAL_STORAGE_KEY) ?? '0') < 5
+    ) {
+      setDisplayEmailModal(true)
+    }
+  }, [])
 
   useMount(() => {
     if (!ARENA_IS_ACTIVE) {
@@ -15,7 +33,14 @@ export default function Page() {
     }
   })
 
-  return ARENA_IS_ACTIVE ? <Arena /> : null
+  return ARENA_IS_ACTIVE ? (
+    <>
+      {displayEmailModal ? (
+        <EmailModal handleCloseModal={() => setDisplayEmailModal(false)} />
+      ) : null}
+      <Arena />
+    </>
+  ) : null
 }
 
 Page.getLayout = function getLayout(page: ReactElement) {
